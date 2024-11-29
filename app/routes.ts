e913181f7a4dc5fdc14928bf4136cb7f181e3680
rafia9005 <rafia9005@gmail.com>
2024-11-29 19:53:11 +0700

    feat(cetak pembayaran): generate cetak pembayaran

--- app/routes.ts
+++ app/routes.ts
@@ -2,4 +2,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   route("/", "routes/home.tsx"),
+  route("/payment", "routes/payment/route.tsx")
 ] satisfies RouteConfig;
